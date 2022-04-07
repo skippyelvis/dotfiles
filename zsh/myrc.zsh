9d148e378fae01ac45ec7b02749e44ab75a16b4d
ja <ja@ja>
2022-04-07 14:12:03 -0700

    aliases

--- zsh/myrc.zsh
+++ zsh/myrc.zsh
@@ -1,5 +1,5 @@
 alias cl="clear"
-alias l="exa --icons"
+alias l="exa --icons -l"
 alias vim="nvim"
 alias p3="python3"
 alias pipi="pip install --user"
@@ -16,4 +16,5 @@ alias sshixd="ssh $UOID@$ixd"
 export EDITOR="nvim"
 export RANGER_LOAD_DEFAULT_RC="FALSE"
 
+export PATH=$PATH:$HOME/cli
 export PATH=$PATH:$HOME/.local/bin
